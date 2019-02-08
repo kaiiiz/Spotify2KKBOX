add66b57e77bd6789fea29efbf763a0fb2355a38
kaiiiz <at881005@gmail.com>
2019-02-09 03:57:13 +0800

    * Modified check auth logic * AJAX upload kbl file * Parse kbl file and store information in session

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-import os
-from flask import Flask, redirect, url_for, session, request, render_template, jsonify
+import os, random, string, xmltodict, requests
+from flask import Flask, redirect, url_for, session, request, render_template, jsonify, send_from_directory
 from flask_dance.contrib.spotify import make_spotify_blueprint, spotify
 from kkbox_auth import make_kkbox_blueprint
 from config import SPOTIFY_APP_ID, SPOTIFY_APP_SECRET, KKBOX_APP_ID, KKBOX_APP_SECRET
-import requests
 
+# Flask config
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'  # Disable HTTPS
+
+# Create Flask Instance
 app = Flask(__name__)
 app.secret_key = 'development'
 
+# OAuth blueprint
 spotify_blueprint = make_spotify_blueprint(
     client_id=SPOTIFY_APP_ID,
     client_secret=SPOTIFY_APP_SECRET,
@@ -23,15 +26,9 @@ app.register_blueprint(spotify_blueprint, url_prefix="/login")
 app.register_blueprint(kkbox_blueprint, url_prefix="/login")
 
 
-@app.route('/')
-def index():
-    spotify_outh_status = True if checkauth('spotify') else False
-    kkbox_outh_status = True if checkauth('kkbox') else False
-    return render_template(
-        'index.html',
-        spotify_outh_status=spotify_outh_status,
-        kkbox_outh_status=kkbox_outh_status,
-    )
+# Utility function
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] == 'kbl'
 
 
 def checkauth(platform):
@@ -40,11 +37,11 @@ def checkauth(platform):
         if token:
             url = 'https://api.spotify.com/v1/me'
             headers = {'Authorization': 'Bearer ' + token}
-            req = requests.get(url, headers=headers).json()
-            if req.get('error'):
-                return False
-            else:
+            req = requests.get(url, headers=headers)
+            if req.status_code == 200:
                 return True
+            else:
+                return False
         else:
             return False
     elif platform == 'kkbox':
@@ -52,13 +49,80 @@ def checkauth(platform):
         if token:
             url = 'https://api.kkbox.com/v1.1/charts'
             headers = {'Authorization': 'Bearer ' + token, 'territory': 'TW'}
-            req = requests.get(url, headers=headers).json()
-            if req.get('error'):
-                return False
-            else:
+            req = requests.get(url, headers=headers)
+            if req.status_code == 200:
                 return True
+            else:
+                return False
         else:
             return False
+
+
+# Web page
+@app.route('/')
+def index():
+    spotify_outh_status = True if checkauth('spotify') else False
+    kkbox_outh_status = True if checkauth('kkbox') else False
+    kbl_kkbox_ver = session[
+        'kbl_kkbox_ver'] if 'kbl_kkbox_ver' in session else 'Unknown'
+    kbl_package_ver = session[
+        'kbl_package_ver'] if 'kbl_package_ver' in session else 'Unknown'
+    kbl_package_descr = session[
+        'kbl_package_descr'] if 'kbl_package_descr' in session else 'Unknown'
+    kbl_package_packdate = session[
+        'kbl_package_packdate'] if 'kbl_package_packdate' in session else 'Unknown'
+    kbl_status = 'Success' if 'kbl_status' in session else 'Unknown'
+    return render_template(
+        'index.html',
+        spotify_outh_status=spotify_outh_status,
+        kkbox_outh_status=kkbox_outh_status,
+        kbl_kkbox_ver=kbl_kkbox_ver,
+        kbl_package_ver=kbl_package_ver,
+        kbl_package_descr=kbl_package_descr,
+        kbl_package_packdate=kbl_package_packdate,
+        kbl_status=kbl_status,
+    )
+
+
+@app.route('/upload_kbl', methods=['POST'])
+def upload_kbl():
+    kbl = {
+        'kkbox_ver': None,
+        'package_ver': None,
+        'package_descr': None,
+        'package_packdate': None,
+    }
+    file = request.files.get('file')
+    if not file:
+        kbl['status'] = "Upload failed! File doesn't exist"
+        return jsonify(kbl=kbl)
+    elif not allowed_file(file.filename):
+        kbl['status'] = "Upload failed! Unsupport file format"
+        return jsonify(kbl=kbl)
+    else:
+        xml = file.read().decode('utf-8')
+        try:
+            content = xmltodict.parse(xml)
+            content = content.get('utf-8_data').get('kkbox_package')
+        except xmltodict.expat.ExpatError:
+            kbl['status'] = 'Upload failed! kbl file parse failed!'
+            return jsonify(kbl=kbl)
+        else:
+            kkbox_ver = content.get('kkbox_ver')
+            package_ver = content.get('package').get('ver')
+            package_descr = content.get('package').get('descr')
+            package_packdate = content.get('package').get('packdate')
+            session['kbl_kkbox_ver'] = kkbox_ver or ''
+            session['kbl_package_ver'] = package_ver or ''
+            session['kbl_package_descr'] = package_descr or ''
+            session['kbl_package_packdate'] = package_packdate or ''
+            session['kbl_status'] = 'Success'
+            kbl['kkbox_ver'] = kkbox_ver
+            kbl['package_ver'] = package_ver
+            kbl['package_descr'] = package_descr
+            kbl['package_packdate'] = package_packdate
+            kbl['status'] = 'Success'
+            return jsonify(kbl=kbl)
 
 
 @app.route('/login', methods=['POST'])
---
+++ static/kkbox.js
@@ -0,0 +1,27 @@
+/*
+ * Function of press the 'Upload' button
+ * This function bind button to an JQuery AJAX function
+ * After click the button, AJAX will upload kbl file and get kbl infomation
+ */
+$(function () {
+    $('#upload_kbl_btn').click(function() {
+        var form_data = new FormData($('#upload_kbl')[0]);
+        $.ajax({
+            type: 'POST',
+            url: '/upload_kbl',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {
+                $('span#kbl_status').html(data.kbl.status)
+                if (data.kbl.status == "Success") {
+                    $('span#kbl_kkbox_ver').html(data.kbl.kkbox_ver)
+                    $('span#kbl_package_ver').html(data.kbl.package_ver)
+                    $('span#kbl_package_descr').html(data.kbl.package_descr)
+                    $('span#kbl_package_packdate').html(data.kbl.package_packdate)
+                }
+            },
+        });
+    });
+});
