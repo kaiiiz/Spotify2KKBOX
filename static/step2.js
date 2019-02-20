bb46fc8b01da97ebb73095c5ae48077539864462
kaiiiz <at881005@gmail.com>
2019-02-21 00:23:49 +0800

    organize js code

--- static/step2.js
+++ static/step2.js
@@ -1,18 +1,10 @@
-/*
- * Function of press the 'Upload' button
- * This function bind button to an JQuery AJAX function
- * After click the button, AJAX will upload kbl file and get kbl information
- */
 $(function () {
-    $('#upload_kbl_btn').click(function() {
+    $('#upload_kbl_btn').click(function () {
         var form_data = new FormData($('#upload_kbl')[0]);
         $.ajax({
             type: 'POST',
             url: '/upload_kbl',
             data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
             success: function (data) {
                 $('span#kbl_status').html(data.kbl.status)
                 if (data.kbl.status == "Success") {
--- static/step4.js
+++ static/step4.js
@@ -64,3 +64,41 @@ function create_frame(playlist_num) {
     }
     $('#convert_detail').html(convert_html)
 }
+// Download
+$(function () {
+    $('#download_btn').click(function () {
+        playlists = get_all_playlists()
+        if (playlists.length == 0) return;
+        $.ajax({
+            type: 'POST',
+            url: '/download/generate_kbl',
+            data: JSON.stringify(playlists),
+            contentType: 'application/json; charset=utf-8',
+            cache: false,
+            processData: false,
+            success: function (data) {
+                window.location.replace('download/' + data.reply.name)
+            },
+        })
+    });
+});
+function get_all_playlists() {
+    playlists = []
+    num = $('#convert_detail').children().length
+    for (let i = 0; i < num; i++) {
+        p = []
+        p_name = $('#convert_success_' + i).prop("name")
+        p_checked = $('#convert_success_' + i).find('input:checked')
+        $.each($('#convert_success_' + i).find('input:checked'), function (i, track) {
+            var track_data = {
+                'song_artist_id': $(track).data('song_artist_id'),
+                'song_album_id': $(track).data('song_album_id'),
+                'song_pathname': $(track).data('song_pathname'),
+                'song_song_idx': $(track).data('song_song_idx')
+            }
+            p.push(track_data)
+        })
+        playlists.push([p_name, p])
+    }
+    return playlists
+}
